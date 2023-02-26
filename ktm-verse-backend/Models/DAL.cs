9d2717cf32123225e14d701542001057145d504f
rohanepal <113876511+rohanepal@users.noreply.github.com>
2023-02-26 12:49:20 +0545

    Backend started
    *created project
    *created database
    * login api started.

---
+++ ktm-verse-backend/Models/DAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+namespace ktm_verse_backend.Models
+{
+	public class DAL
+	{
+		public Response register(Users users, SqlConnection connection)
+		{
+			Response response = new Response(); 
+
+
+
+			return response; 
+		}
+	}
+}
+
---
+++ ktm-verse-backend/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ktm_verse_backend.Models
+{
+	public class Response
+	{
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+
+        public List<Users> listUsers { get; set; }
+
+        public Users user { get; set; }
+    }
+}
+
